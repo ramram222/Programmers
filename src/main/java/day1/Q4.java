828049bab14f3add66b7d3112815dc2e74b9d115
ramram222 <kim.ram454@gmail.com>
2023-03-30 21:16:03 +0900

    feat: day1 - 몫 구하기

---
+++ src/main/java/day1/Q4.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class Q4 {
+    // 몫 구하기
+    public static void main(String[] args) {
+        int num1 = 7;
+        int num2 = 2;
+        System.out.println(solution(num1, num2));
+    }
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+        int min = 0;
+        int max = 100;
+
+        if(min <= num1 && num1 <= max && min <= num2 && num2 <= max) {
+            answer = num1 / num2;
+        }
+        return answer;
+    }
+}
