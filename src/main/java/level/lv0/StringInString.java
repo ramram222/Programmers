b9ec6df6886162df5bbba95e0d14de5c19044edf
ramram222 <kim.ram454@gmail.com>
2023-04-17 23:56:27 +0900

    feat: lv0 - 문자열안에 문자열

---
+++ src/main/java/level/lv0/StringInString.java
@@ -0,0 +1,19 @@
+package level.lv0;
+
+public class StringInString {
+    // 문자열안에 문자열
+    public static void main(String[] args) {
+        String str1 = "ab6CDE443fgh22iJKlmn1o";
+        String str2 = "6CD";
+        System.out.println(solution(str1, str2));
+    }
+    public static int solution(String str1, String str2) {
+        int answer = 0;
+        if (str1.contains(str2)) {
+            answer = 1;
+        } else {
+            answer = 2;
+        }
+        return answer;
+    }
+}
