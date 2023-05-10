1dc40587aad330e994c078354788a3d094878651
ramram222 <kim.ram454@gmail.com>
2023-05-11 00:19:40 +0900

    feat: Lv0 - 최댓값 만들기 (1)

---
+++ src/main/java/level/lv0/TheBiggest.java
@@ -0,0 +1,19 @@
+package level.lv0;
+
+import java.util.Arrays;
+
+public class TheBiggest {
+    // 최댓값 만들기 (1)
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println(solution(numbers));
+    }
+    public static int solution(int[] numbers) {
+        int answer = 0;
+        Arrays.sort(numbers);
+
+        answer = numbers[numbers.length - 1] * numbers[numbers.length - 2];
+
+        return answer;
+    }
+}
