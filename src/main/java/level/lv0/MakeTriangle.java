55a7df465fe6313925e228799b647680bceb3763
ramram222 <kim.ram454@gmail.com>
2023-05-11 00:11:01 +0900

    feat: Lv0 - 삼각형의 완성조건(1)

---
+++ src/main/java/level/lv0/MakeTriangle.java
@@ -0,0 +1,20 @@
+package level.lv0;
+
+import java.util.Arrays;
+
+public class MakeTriangle {
+    // 삼각형의 완성조건 (1)
+    public static void main(String[] args) {
+        int[] sides = {1, 2, 3};
+        System.out.println(solution(sides));
+    }
+    public static int solution(int[] sides) {
+        int answer = 0;
+        int max = Arrays.stream(sides).max().getAsInt();
+        int totalLength = Arrays.stream(sides).sum();
+
+        answer = totalLength - max > max ? 1 : 2;
+
+        return answer;
+    }
+}
