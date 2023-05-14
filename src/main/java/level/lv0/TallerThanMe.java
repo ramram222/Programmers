844da95d5ce86f32ca8cd56800b7c1629ea5191c
ramram222 <kim.ram454@gmail.com>
2023-05-14 23:37:04 +0900

    feat: Lv0 - 머쓱이보다 키 큰 사람

---
+++ src/main/java/level/lv0/TallerThanMe.java
@@ -0,0 +1,17 @@
+package level.lv0;
+
+public class TallerThanMe {
+    // 머쓱이보다 키 큰 사람
+    public static void main(String[] args) {
+        int[] array = {49, 180, 192, 170};
+        int height = 167;
+        System.out.println(solution(array, height));
+    }
+    public static int solution(int[] array, int height) {
+        int answer = 0;
+        for (int num: array) {
+            if (num > height) answer++;
+        }
+        return answer;
+    }
+}
