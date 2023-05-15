0cbf0caa8f72f7b2b6906ead7737378df2b96154
ramram222 <kim.ram454@gmail.com>
2023-05-16 01:13:41 +0900

    feat: day10 - 합성수 찾기

---
+++ src/main/java/daily/day11/Q2.java
@@ -0,0 +1,20 @@
+package daily.day11;
+
+public class Q2 {
+    // 합성수 찾기
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(solution(n));
+    }
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) cnt++;
+            }
+            if (cnt > 2) answer++;
+        }
+        return answer;
+    }
+}
