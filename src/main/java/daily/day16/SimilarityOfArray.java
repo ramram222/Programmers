485ac4d029c0389bb7b6ac6d7ea47cfe61846aad
ramram222 <kim.ram454@gmail.com>
2023-05-29 23:07:12 +0900

    feat: day16 - 배열의 유사도

---
+++ src/main/java/daily/day16/SimilarityOfArray.java
@@ -0,0 +1,19 @@
+package daily.day16;
+
+public class SimilarityOfArray {
+    public static void main(String[] args) {
+        // 배열의 유사도
+        String[] s1 = {"a", "b", "c"};
+        String[] s2 = {"com", "b", "d", "p", "c"};
+        System.out.println(solution(s1, s2));
+    }
+    public static int solution(String[] s1, String[] s2) {
+        int answer = 0;
+        for (int i = 0; i < s1.length; i++) {
+            for (int j = 0; j < s2.length; j++) {
+                if (s1[i].equals(s2[j])) answer += 1;
+            }
+        }
+        return answer;
+    }
+}
