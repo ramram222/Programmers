83afb02304963c6b7f4f8a69379cd16a1a92aa26
ramram222 <kim.ram454@gmail.com>
2023-06-01 23:50:45 +0900

    feat: day17 - 자릿수 더하기

---
+++ src/main/java/daily/day17/plusAll.java
@@ -0,0 +1,17 @@
+package daily.day17;
+
+public class plusAll {
+    // 자릿수 더하기
+    public static void main(String[] args) {
+        int n = 1234;
+        System.out.println(solution(n));
+    }
+    public static int solution(int n) {
+        int answer = 0;
+        String[] numList = String.valueOf(n).split("");
+        for (String num: numList) {
+            answer += Integer.parseInt(num);
+        }
+        return answer;
+    }
+}
